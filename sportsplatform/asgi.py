e9341a0cd35fdd928b99f0f2bae8e68b5f7c7d39
Danilo <danilo.koljensic.fit@gmail.com>
2026-06-08 14:36:37 +0200

    project setup

---
+++ sportsplatform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sportsplatform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportsplatform.settings')
+
+application = get_asgi_application()
