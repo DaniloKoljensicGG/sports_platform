e9fafc49c9376d650b9387f09d84fbddf9960c96
Danilo <danilo.koljensic.fit@gmail.com>
2026-06-15 14:19:20 +0200

    Calculating age of player and adding into serializer

--- players/models.py
+++ players/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Sport(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class PlayerProfile(models.Model):
     GENDER_MALE = "M"
--- players/serializers.py
+++ players/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
 from .models import PlayerProfile, Sport
+from datetime import date
 
 
 class PlayerProfileSerializer(serializers.ModelSerializer):
+    age = serializers.SerializerMethodField()
+
     class Meta:
         model = PlayerProfile
         fields = "__all__"
 
-    # def age(self, ):
-    #     today =
+    def get_age(self, obj):
+        if not obj.date_of_birth:
+            return None
+
+        today = date.today()
+
+        age = (
+            today.year
+            - obj.date_of_birth.year
+            - (
+                (today.month, today.day)
+                < (obj.date_of_birth.month, obj.date_of_birth.day)
+            )
+        )
+        return age
 
 
 class SportSerializer(serializers.ModelSerializer):
